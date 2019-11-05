89ef79a3c792cc21babcab1ad541477fe3659ead
Sawaki <you@example.com>
2019-11-05 16:49:46 +0900

    追加機能の実装とデバック用の記述削除

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default function Form(props) {
-    const [fuck, setFuck] = React.useState(0)
     function handleSubmit(e) {
         e.preventDefault()
         const newTodos = props.todos.slice()
@@ -9,23 +8,14 @@ export default function Form(props) {
             id: props.todos.length,
             content: e.target.desc.value
         })
-        setFuck(fuck + 2)
+        
         props.setTodos(newTodos)
-        console.log(newTodos)
-        console.log(fuck)
-    }
-    function handleClick() {
-        setFuck(fuck + 2)
-        console.log(fuck)
     }
 
     return (
-        <div>
             <form onSubmit={handleSubmit}>
                 <input type="text" name="desc"></input>
                 <button type="submit">追加</button>
             </form>
-            <button onClick={handleClick}>うおおおおお</button>
-        </div>
     )
 }
